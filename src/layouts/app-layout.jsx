52a065843158cdcffe21673c3bfda2e846a89f64
Om Kumar <omrajputrajput4@gmail.com>
2026-01-16 23:17:11 +0530

    Your commit message

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -4,8 +4,8 @@ import {Outlet} from "react-router-dom";
 const AppLayout = () => {
   return (
     <div>
-      {/* <main className="min-h-screen container"> */}
-      <main className="min-h-screen max-w-screen-2xl mx-auto px-6 scale-[1.02] origin-center">
+      <main className="min-h-screen container">
+      {/* <main className="min-h-screen max-w-screen-2xl mx-auto px-6 scale-[1.02] origin-center"> */}
 
       {/* <main className="min-h-screen w-full max-w-screen-2xl mx-auto px-6"> */}
 
